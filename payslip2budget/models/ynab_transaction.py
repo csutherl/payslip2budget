440f5534156708b4f77193f1a15741598537332b
Coty Sutherland <sutherland.coty@gmail.com>
2025-05-14 16:38:23 -0400

    Since the transactions need to be approved we can go ahead and mark them as cleared too

--- payslip2budget/models/ynab_transaction.py
+++ payslip2budget/models/ynab_transaction.py
@@ -8,7 +8,7 @@ class YNABTransaction(Transaction):
     account_id: str
     category_id: Optional[str] = None
     category_name: Optional[str] = None
-    cleared: str = "uncleared"  # 'cleared', 'uncleared', or 'reconciled'
+    cleared: str = "cleared"  # 'cleared', 'uncleared', or 'reconciled'
     approved: bool = False # True or False
     flag_color: Optional[str] = None  # e.g., "red", "orange", etc.
     import_id: Optional[str] = None
